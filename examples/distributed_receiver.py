17d8ea2ab60b1b0cb872197491053e6ba1064aeb
Vladimir Leskov <tinsane@nebius.com>
2025-01-08 22:30:21 +0100

    Example transfer for testing

---
+++ examples/distributed_receiver.py
@@ -0,0 +1,27 @@
+import torch
+
+from vllm.config import KVTransferConfig
+from vllm.distributed.kv_transfer.kv_pipe.pynccl_pipe import PyNcclPipe
+
+
+device = torch.device("cuda")
+
+config = KVTransferConfig(
+    kv_connector='PyNcclConnector',
+    kv_buffer_device='cuda',
+    kv_buffer_size=1e9,
+    kv_rank=1,
+    kv_role="kv_both",  # this arg doesn't matter in this test
+    kv_parallel_size=2,
+    kv_ip="192.168.0.28",
+    kv_port=29500,
+)
+
+pipe = PyNcclPipe(
+    local_rank=device.index,
+    config=config,
+    device="cuda",
+)
+
+tensor = pipe.recv_tensor()
+print("Received tensor with shape: {}".format(tensor.shape))
---
+++ examples/distributed_sender.py
@@ -0,0 +1,28 @@
+import torch
+
+from vllm.config import KVTransferConfig
+from vllm.distributed.kv_transfer.kv_pipe.pynccl_pipe import PyNcclPipe
+
+
+device = torch.device("cuda")
+
+config = KVTransferConfig(
+    kv_connector='PyNcclConnector',
+    kv_buffer_device='cuda',
+    kv_buffer_size=1e9,
+    kv_rank=0,
+    kv_role="kv_both",  # this arg doesn't matter in this test
+    kv_parallel_size=2,
+    kv_ip="192.168.0.28",
+    kv_port=29500,
+)
+
+pipe = PyNcclPipe(
+    local_rank=device.index,
+    config=config,
+    device="cuda",
+)
+
+tensor = torch.randn(131072, 131072, device=device)
+pipe.send_tensor(tensor)
+print("Sent tensor with shape: {}".format(tensor.shape))
