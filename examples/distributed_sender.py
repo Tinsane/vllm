ed013f5b2c42f97dec24f70bebfcc8fc252f738e
Vladimir Leskov <tinsane@nebius.com>
2025-01-10 17:20:01 +0100

    Fix

--- examples/distributed_sender.py
+++ examples/distributed_sender.py
@@ -13,7 +13,7 @@ config = KVTransferConfig(
     kv_rank=0,
     kv_role="kv_both",  # this arg doesn't matter in this test
     kv_parallel_size=2,
-    kv_ip="192.168.0.28",
+    kv_ip="192.168.0.145",
     kv_port=29500,
 )
 
