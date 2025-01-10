22b2f53bd3a89102b99715fd4f7821f2972f3de8
Vladimir Leskov <tinsane@nebius.com>
2025-01-10 10:42:31 +0100

    Fix

--- vllm/model_executor/model_loader/infiniband.py
+++ vllm/model_executor/model_loader/infiniband.py
@@ -69,7 +69,7 @@ class InfinibandModelLoader:
             kv_role="kv_both",  # this arg doesn't matter in this test
             kv_parallel_size=2,
             kv_ip="192.168.0.145",
-            kv_port=29500,
+            kv_port=29503,
         )
 
         pipe = PyNcclPipe(
