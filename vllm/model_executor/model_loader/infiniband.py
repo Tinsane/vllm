ae7d478f77304d74006046997ee6e5e70d81c0ba
Vladimir Leskov <tinsane@nebius.com>
2025-01-14 20:48:10 +0100

    Fix

--- vllm/model_executor/model_loader/infiniband.py
+++ vllm/model_executor/model_loader/infiniband.py
@@ -117,7 +117,7 @@ class InfinibandModelLoader:
 
     def fetch_model_weights(self, model: torch.nn.Module):
         state = model.state_dict()
-        for name, tensor in state:
+        for name, tensor in state.items():
             logger.debug(f"Fetching tensor {name} with shape {tensor.shape}")
         for name, tensor in self.load_tensors():
             assert (name in state), f"Unexpected tensor {name}"
