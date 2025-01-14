673d38fa168892b8e6cbe4f37f99c83a1ca6cc38
Vladimir Leskov <tinsane@nebius.com>
2025-01-14 21:49:44 +0100

    Fix

--- vllm/model_executor/model_loader/infiniband.py
+++ vllm/model_executor/model_loader/infiniband.py
@@ -17,9 +17,9 @@ class InfinibandModelLoader:
     def _send_tensor(self, pipe: PyNcclPipe, name: str, tensor: torch.Tensor):
         while True:
             torch.cuda.synchronize()
-            check_sum = torch.sum(tensor, dtype=torch.bfloat16).to(device="cpu")
+            check_sum = torch.sum(tensor.to(dtype=torch.bfloat16)).to(device="cpu")
             torch.cuda.synchronize()
-            logger.debug(f"Sending tensor {name}, {tensor.shape}, {tensor.dtype}, {check_sum.dtype}, {check_sum}")
+            logger.debug(f"Sending tensor {name}, {tensor.shape}, {tensor.dtype}, {check_sum.dtype}, {check_sum}, {tensor.flatten()[:5]}")
             meta = pipe.send_tensor_with_response(tensor, metadata={
                 "finished": torch.zeros((1,), dtype=torch.bool, device='cpu'),
                 "name": torch.tensor(list(name.encode('u8')), dtype=torch.uint8, device="cpu"),
@@ -90,9 +90,9 @@ class InfinibandModelLoader:
             name = bytes(name_raw.numpy()).decode('u8')
             check_sum = metadata['check_sum']
             torch.cuda.synchronize()
-            real_sum = torch.sum(tensor, dtype=torch.bfloat16).to(device="cpu")
+            real_sum = torch.sum(tensor.to(dtype=torch.bfloat16)).to(device="cpu")
             torch.cuda.synchronize()
-            logger.debug(f"Receiving tensor {name}, {tensor.shape}, {tensor.dtype}, {check_sum.dtype}, {check_sum}, {real_sum.dtype}, {real_sum}")
+            logger.debug(f"Receiving tensor {name}, {tensor.shape}, {tensor.dtype}, {check_sum.dtype}, {check_sum}, {real_sum.dtype}, {real_sum}, {tensor.flatten()[:5]}")
             logger.debug("Check sum difference: {}".format(check_sum - real_sum))
             if abs(check_sum - real_sum) < 1e-6:
                 pipe.send_metadata_only({
