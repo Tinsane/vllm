4fbf962ba1fa1b0b99a38a55fa71114513fb517f
Vladimir Leskov <tinsane@nebius.com>
2025-01-14 20:42:29 +0100

    Fix

--- vllm/model_executor/model_loader/weight_utils.py
+++ vllm/model_executor/model_loader/weight_utils.py
@@ -415,6 +415,7 @@ def safetensors_weights_iterator(
         with safe_open(st_file, framework="pt") as f:
             for name in f.keys():  # noqa: SIM118
                 param = f.get_tensor(name)
+                logger.debug(f"Loading {name} weight with shape {param.shape}")
                 yield name, param
 
 
